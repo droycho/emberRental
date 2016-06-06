34bc355329d683fb14a3ccb15cbeefe26fc7d4a1
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-06 16:22:57 -0700

    link app to firebase.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://super-rentals-8fd36.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
